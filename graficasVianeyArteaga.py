535311b873350a9c9a17e8949864268eeada327b
VianeyArteaga <choisoobinmx@gmail.com>
2025-02-26 09:41:57 -0700

    Agregado Graficas

--- graficasVianeyArteaga.py
+++ graficasVianeyArteaga.py
@@ -1,7 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
-plt.plot(xpoints,   ypoints)
+ 
+#x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+#y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+ 
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+ 
+xhist = np.random.normal(170, 10, 250)
+ 
+#Gráfica de lineas
+#plt.plot(x, y)
+#Gráfica de dispersión
+#plt.scatter(x, y)
+#Gráfica de barras
+#plt.bar(x,y)
+#Histograma
+plt.hist(xhist)
 plt.show()
+ 
